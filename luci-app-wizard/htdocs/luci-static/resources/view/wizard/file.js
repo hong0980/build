9b3d8e8084ebfb9c66cc575ab71e210f6565cebd
hong0980 <hong0980@qq.com>
2025-05-20 21:29:13 +0800

    update@2025-05-20-21-29

--- luci-app-wizard/htdocs/luci-static/resources/view/wizard/file.js
+++ luci-app-wizard/htdocs/luci-static/resources/view/wizard/file.js
@@ -3,32 +3,29 @@
 'require fs';
 'require ui';
 
-var isReadonlyView = !L.hasViewPermission() || null;
 var configFiles = [
-	'/etc/config/network',
-	'/etc/config/firewall',
-	'/etc/config/dhcp',
-	'/etc/dnsmasq.conf',
-	'/etc/config/uhttpd',
-	'/etc/config/wireless',
-	'/etc/hosts',
-	'/etc/rc.local',
-	'/etc/crontabs/root'
-].map(function(path) {
-	var fileName = path.split('/').pop();
-	var tabName = fileName.includes('.') ? fileName.split('.').shift() : fileName;
-	return {
-		path: path,
-		data_tab: tabName,
-		title: fileName.replace(/^./, function(c) { return c.toUpperCase(); })
-	};
-});
+	{ path: '/etc/config/network', title: _('网络配置（network）') },
+	{ path: '/etc/config/firewall', title: _('防火墙配置（firewall）') },
+	{ path: '/etc/config/dhcp', title: _('DHCP配置（dhcp）') },
+	{ path: '/etc/dnsmasq.conf', title: _('DNS配置（dnsmasq）') },
+	{ path: '/etc/config/uhttpd', title: _('Web服务器配置（uhttpd）') },
+	{ path: '/etc/config/wireless', title: _('无线配置（wireless）') },
+	{ path: '/etc/hosts', title: _('主机配置（hosts）') },
+	{ path: '/etc/rc.local', title: _('启动脚本') },
+	{ path: '/etc/crontabs/root', title: _('定时任务（crontabs）') }
+];
 
 return view.extend({
 	load: function() {
-		return Promise.all([
-			...configFiles.map(function(config) { return L.resolveDefault(fs.read(config.path), null)})
-		]);
+		return Promise.all(
+			configFiles.map(function(r) {
+				return fs.stat(r.path)
+					.then(function() { return r })
+					.catch(function() { return null });
+			})
+		).then(function(p) {
+			return p.filter(Boolean);
+		});
 	},
 
 	handleFileSave: function(path, textareaId) {
@@ -37,19 +34,20 @@ return view.extend({
 			ui.addNotification(null, E('p',
 				_('Contents of %s have been saved.').format(path)), 'info');
 
-			var service = path.includes('crontabs') ? 'cron' :
-						 path.includes('dhcp') ? 'dnsmasq' :
-						 path.includes('wireless') ? 'wifi' :
-						 path.includes('uhttpd') ? 'uhttpd' :
-						 path.includes('network') ? 'network' :
-						 path.includes('dnsmasq') ? 'dnsmasq' :
-						 path.includes('firewall') ? 'firewall' : null;
-			if (service) {
-				var cmd = service === 'wifi' ? '/sbin/wifi' : '/etc/init.d/' + service;
-				var args = service === 'wifi' ? ['reload'] : ['reload'];
-				return fs.exec_direct(cmd, args).then(function() {
+			var s = path.includes('crontabs') ? 'cron' :
+					path.includes('dhcp') ? 'dnsmasq' :
+					path.includes('hosts') ? 'dnsmasq' :
+					path.includes('wireless') ? 'wifi' :
+					path.includes('uhttpd') ? 'uhttpd' :
+					path.includes('network') ? 'network' :
+					path.includes('dnsmasq') ? 'dnsmasq' :
+					path.includes('firewall') ? 'firewall' : null;
+			if (s) {
+				var c = s === 'wifi' ? '/sbin/wifi' : '/etc/init.d/' + s;
+				var a = s === 'wifi' ? ['reload'] : ['reload'];
+				return fs.exec_direct(c, a).then(function() {
 					ui.addNotification(null, E('p',
-						_('Service %s reloaded successfully.').format(cmd)), 'info');
+						_('Service %s reloaded successfully.').format(c)), 'info');
 				}).catch(function(err) {
 					ui.addNotification(null, E('p',
 						_('Service reload failed: %s').format(err.message)), 'warning');
@@ -63,61 +61,99 @@ return view.extend({
 
 	render: function(data) {
 		var self = this;
-		var tabContents = configFiles.map(function(config, index) {
-			var content = data[index];
-			return content ?
-				E('div', { 'class': 'cbi-tab', 'data-tab': config.data_tab, 'data-tab-title': _('%s Configuration File').format(config.title)}, [
-					E('p', {}, _("This page contains the configuration file content for <code>%s</code>. After editing, click the <b><font color='red'>Save</font></b> button to apply changes immediately.").format(config.path)),
-					E('div', { 'class': 'cbi-checkbox', 'style': 'display: flex; justify-content: flex-end; margin-bottom: 10px;' }, [
-						E('label', { 'style': 'margin-right: 10px;' }, [
-							E('font', { 'color': 'red' }, _('Enable editing')),
-						]),
-						E('input', {
-							'type': 'checkbox',
-							'id': 'edit_' + config.data_tab,
-							'disabled': isReadonlyView,
-							'change': function(ev) {
-								var textarea = document.getElementById(config.data_tab);
-								var saveButton = document.getElementById('save_' + config.data_tab);
-								saveButton.style.display = ev.target.checked ? 'inline-block' : 'none';
-								textarea.readOnly = !ev.target.checked;
-							}
-						})
-					]),
-					E('textarea', {
-						'rows': 22,
-						'id': config.data_tab,
-						'readonly': true,
-						'style': 'width:100%; background-color:#272626; color:#c5c5b2; border:1px solid #555; font-family:Consolas, monospace; font-size:14px;'
-					}, [content]),
-					E('div', { 'class': 'cbi-page-actions' }, [
-						E('button', {
-							'id': 'save_' + config.data_tab,
-							'class': 'btn cbi-button-save',
-							'disabled': isReadonlyView,
-							'style': 'display: none;',
-							'click': ui.createHandlerFn(self, 'handleFileSave', config.path, config.data_tab)
-						}, _('Save'))
-					])
-				]) : null;
-		}).filter(function(item) { return !!item; });
-
-		var view = E('div', {}, [
+		self.originalContent = '';
+		var view = E('div', { 'class': 'cbi-section' }, [
 			E('b', {}, [
 				E('font', { 'color': 'red' }, _('The configuration file is directly edited and saved! Unless you know what you are doing, please do not modify these configuration files. Incorrect configurations may cause issues such as failure to boot or network errors.')),
 				E('br'),
 				E('font', { 'color': 'green' }, _('It is recommended to back up the file before making changes. Comments can be added by starting a line with #.'))
 			]),
-			E('div', { 'class': 'cbi-tab-container' }, tabContents)
+			E('div', { 'class': 'cbi-value' }, [
+				E('label', { 'class': 'cbi-value-title' }, _('Choose File')),
+				E('div', { 'class': 'cbi-value-field' }, [
+					E('select', {
+						'id': 'file_select',
+						'class': 'cbi-input-select',
+						'change': function(ev) {
+							var value = ev.target.value;
+							var textarea = document.getElementById('file_content');
+							var editToggle = document.getElementById('edit_toggle');
+							textarea.value = '';
+							self.originalContent = '';
+							fs.read(value)
+								.then(function(content) {
+									textarea.value = content;
+									self.originalContent = content;
+									textarea.readOnly = editToggle.checked;
+								})
+								.catch(function(err) {
+									ui.addNotification(null, E('p',
+										_('Unable to read file: %s').format(err.message)), 'error');
+								});
+						}
+					}, [
+						E('option', { 'value': '' }, _('-- Please choose --')),
+						...data.map(function(config) {
+							return E('option', { 'value': config.path }, config.title);
+						})
+					])
+				])
+			]),
+			E('div', { 'class': 'cbi-value' }, [
+				E('div', { 'class': 'cbi-checkbox', 'style': 'display: flex; justify-content: flex-end; margin-bottom: 10px; width: 100%;' }, [
+					E('label', { 'style': 'margin-right: 10px;' }, [
+						E('font', { 'color': 'red' }, _('Readonly'))
+					]),
+					E('input', {
+						'type': 'checkbox',
+						'id': 'edit_toggle',
+						'checked': true,
+						'change': function(ev) {
+							var isChecked = ev.target.checked;
+							var textarea = document.getElementById('file_content');
+							var buttons = [document.getElementById('save_button'), document.getElementById('reset_button')];
+							textarea.readOnly = isChecked;
+							buttons.forEach(function(button) {
+								button.style.display = !isChecked ? 'inline-block' : 'none';
+							});
+						}
+					})
+				]),
+				E('div', { 'style': 'width:100%' }, [
+					E('textarea', {
+						'rows': 22,
+						'id': 'file_content',
+						'style': 'width:100%; background-color:#272626; color:#c5c5b2; border:1px solid #555; font-family:Consolas, monospace; font-size:14px;',
+						'readonly': true
+					}, '')
+				])
+			]),
+			E('div', { 'class': 'cbi-page-actions',  }, [
+				E('button', {
+					'id': 'save_button',
+					'class': 'btn cbi-button-save',
+					'style': 'display: none; margin-right: 10px;',
+					'click': ui.createHandlerFn(self, function() {
+						var path = document.getElementById('file_select').value;
+						if (!path) {
+							ui.addNotification(null, E('p', _('Please select a file.')), 'error');
+							return;
+						}
+						return self.handleFileSave(path, 'file_content');
+					})
+				}, _('Save')),
+				E('button', {
+					'id': 'reset_button',
+					'style': 'display: none',
+					'class': 'btn cbi-button-reset',
+					'click': ui.createHandlerFn(self, function() {
+						var textarea = document.getElementById('file_content');
+						textarea.value = self.originalContent;
+					})
+				}, _('Reset'))
+			])
 		]);
 
-		setTimeout(function() {
-			document.querySelectorAll('input[id^="edit_"]').forEach(function(checkbox) {
-				if (!checkbox.disabled) checkbox.checked = false;
-			});
-		}, 0);
-
-		ui.tabs.initTabGroup(view.lastElementChild.childNodes);
 		return view;
 	},
 
