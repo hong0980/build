88c3126e725eaed51065eca5e50f36ad5ab6ba12
hong0980 <hong0980@qq.com>
2025-05-27 18:54:42 +0800

    update@2025-05-27-18-54

---
+++ luci-app-easymesh/luasrc/controller/easymesh.lua
@@ -0,0 +1,18 @@
+-- Copyright (C) 2021 dz <dingzhong110@gmail.com>
+
+module("luci.controller.easymesh", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/easymesh") then
+		return
+	end
+
+	if not nixio.fs.access("/etc/config/wireless") then
+		return
+	end
+
+	local page
+	page = entry({"admin", "network", "easymesh"}, cbi("easymesh"), _("EASY MESH"), 60)
+	page.dependent = true
+	page.acl_depends = { "luci-app-easymesh" }
+end
