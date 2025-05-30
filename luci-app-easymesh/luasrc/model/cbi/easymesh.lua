bd488c25313b5a9474ffd792a6d9f41f69e1c764
hong0980 <hong0980@qq.com>
2025-05-31 02:07:36 +0800

    update@2025-05-31-02-07

--- luci-app-easymesh/luasrc/model/cbi/easymesh.lua
+++ luci-app-easymesh/luasrc/model/cbi/easymesh.lua
@@ -1,21 +1,17 @@
 -- Copyright (C) 2021 dz <dingzhong110@gmail.com>
-
+-- https://openwrt.org/docs/guide-user/network/wifi/mesh/batman
 local m, s, o
 local util = require "luci.util"
 local uci  = require "luci.model.uci".cursor()
 
 local function get_bat_nodes()
 	local nodes = {}
-	local output = util.exec("batctl n 2>/dev/null | awk 'NR>2 {print $1,$2,$3}'") or ""
+	local output = util.exec("batctl n 2>/dev/null | awk 'NR>2 {print $1,$2,$3,$4}'") or ""
 
 	for line in output:gmatch("[^\r\n]+") do
-		local iface, neighbor, lastseen = line:match("^(%S+)%s+(%S+)%s+(%S+)")
+		local iface, neighbor, lastseen, link_quality = line:match("^(%S+)%s+(%S+)%s+(%S+)%s+(%S+)")
 		if iface then
-			nodes[#nodes + 1] = {
-				IF = iface,
-				Neighbor = neighbor,
-				lastseen = lastseen
-			}
+			nodes[#nodes + 1] = { IF = iface, Neighbor = neighbor, lastseen = lastseen, link_quality = link_quality }
 		end
 	end
 
@@ -28,19 +24,18 @@ local node_count = #bat_nodes
 m = Map("easymesh", translate("EasyMesh Configuration"),
 	translate("Configure wireless mesh network using Batman-adv"))
 
--- 节点状态显示
 if node_count > 0 then
 	s = m:section(Table, bat_nodes,
-		translatef("<b>Active Mesh Nodes: %d</b>" %node_count))
+		translatef("<b>Active Mesh Nodes: %d</b>", node_count))
 	s:option(DummyValue, "IF", translate("Interface"))
 	s:option(DummyValue, "Neighbor", translate("Neighbor MAC"))
 	s:option(DummyValue, "lastseen", translate("Last Seen"))
+	s:option(DummyValue, "link_quality", translate("Link Quality"))
 end
--- Basic
+
 s = m:section(TypedSection, "easymesh", translate(" "))
 s.anonymous = true
 
----- Enable
 o = s:option(Flag, "enabled", translate("Enable"),
 	translate("Enable or disable EASY MESH"))
 o.default = 0
@@ -52,7 +47,6 @@ o:value("server", translate("host MESH"))
 o:value("client", translate("son MESH"))
 o.rmempty = false
 
--- 动态无线设备检测
 local radios = {}
 uci:foreach("wireless", "wifi-device",
 	function(s) radios[s['.name']] = true end)
@@ -65,26 +59,24 @@ end
 o:value("all", translate("All"))
 o.default = next(radios) or "radio0"
 
----- mesh
 o = s:option(Value, "mesh_id", translate("MESH ID"),
 	translate("MESH ID"))
 o.default = "openwrt_mesh"
 o.datatype = "string"
 
-enable = s:option(Flag, "encryption", translate("Encryption"))
-enable.default = 0
-enable.rmempty = false
+o = s:option(Flag, "encryption", translate("Encryption"))
+o.default = 0
+o.rmempty = false
 
 o = s:option(Value, "key", translate("Key"))
 o.default = "easymesh"
 o:depends("encryption", 1)
 o.datatype = "rangelength(8,16)"
 
----- kvr
-enable = s:option(Flag, "kvr", translate("K/V/R"))
-enable.default = 1
-enable.rmempty = false
-enable.description = translate("Enable 802.11k/v/r for improved roaming and network management. 802.11k provides neighbor reports, 802.11v supports load balancing, and 802.11r enables fast AP transitions.")
+o = s:option(Flag, "kvr", translate("K/V/R"),
+	translate("Enable 802.11k/v/r for improved roaming and network management. 802.11k provides neighbor reports, 802.11v supports load balancing, and 802.11r enables fast AP transitions."))
+o.default = 1
+o.rmempty = false
 
 o = s:option(Value, "mobility_domain", translate("Mobility Domain"),
 	translate("4-character hexadecimal ID"))
