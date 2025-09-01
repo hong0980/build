a1cc4e68d67780745cf98cb3c8d1fc9d035ca9f9
hong0980 <hong0980@qq.com>
2025-09-02 03:23:27 +0800

    update@2025-09-02-03-23

--- luci-app-wizard/htdocs/luci-static/resources/view/wizard/file.js
+++ luci-app-wizard/htdocs/luci-static/resources/view/wizard/file.js
@@ -3,6 +3,7 @@
 'require ui';
 'require view';
 
+const d = {};
 const notify = L.bind(ui.addTimeLimitedNotification || ui.addNotification, ui);
 const configFiles = [
 	{ path: '/etc/config/network', 	title: _('Network configuration (network)') },
@@ -26,11 +27,8 @@ return view.extend({
 		.then((data) => Object.fromEntries(data.filter(Boolean).map(c => [c.path, c]))),
 
 	render: function (data) {
-		this.path = '';
-		this.content = '';
-		const dom = {};
-		const fileStatusDiv = E('span', { style: 'color:#888;font-size:90%;' });
-		const view = E('div', {}, [
+		const filestatus = E('span', { style: 'color:#888;font-size:90%;' });
+		const view = E('div', [
 			E('font', { color: 'red', style: 'font-weight: bold;' },
 				_('The configuration file is directly edited and saved! Unless you know what you are doing, please do not modify these configuration files. Incorrect configurations may cause issues such as failure to boot or network errors.')),
 			E('div', { class: 'cbi-value', style: 'align-items: center;' }, [
@@ -39,18 +37,15 @@ return view.extend({
 					E('select', {
 						class: 'cbi-input-select',
 						change: ui.createHandlerFn(this, (ev) => {
-							fileStatusDiv.innerHTML = '';
 							const filepath = ev.target.value;
 							filepath && fs.read(filepath).then(content => {
-								const selectedset = data[filepath];
-								if (selectedset) {
-									fileStatusDiv.innerHTML = _('Last modified: %s, Size: %s bytes').format(
-										new Date(selectedset.stat.mtime * 1000).toLocaleString(), selectedset.stat.size);
-								};
-								this.path = filepath;
-								this.content = content;
-								dom.textarea.value = content;
-							})
+								const set = data[filepath];
+								filestatus.textContent = _('Last modified: %s, Size: %s bytes').format(
+									new Date(set.stat.mtime * 1000).toLocaleString(), set.stat.size);
+								d.path = filepath;
+								d.oldcontent = content;
+								d.textarea.value = content;
+							});
 						})
 					}, [
 						E('option', { value: '' }, _('-- Please choose --')),
@@ -62,9 +57,9 @@ return view.extend({
 					E('input', {
 						type: 'checkbox', style: 'margin-left: 12px;', checked: true,
 						change: ui.createHandlerFn(this, (ev) => {
-							var isChecked = ev.target.checked;
-							dom.textarea.readOnly = isChecked;
-							document.getElementById('page_actions').style.display = isChecked ? 'none' : 'block';
+							const isChecked = ev.target.checked;
+							d.textarea.readOnly = isChecked;
+							d.actions.style.display = isChecked ? 'none' : 'block';
 						})
 					})
 				])
@@ -76,58 +71,55 @@ return view.extend({
 			E('div', { class: 'cbi-value-description' }, [
 				E('font', { color: 'green', style: 'font-weight: bold;' },
 					_('It is recommended to back up the file before making changes. Comments can be added by starting a line with #.')),
-				fileStatusDiv,
+				filestatus,
 			]),
-			E('div', { class: 'cbi-page-actions', style: 'display: none;', id: 'page_actions' }, [
-				E('div', {
-					class: 'btn cbi-button-save', style: 'margin-right: 10px;',
-					click: ui.createHandlerFn(this, () => {
-						const path = dom.select.value || this.path;
-						const value = dom.textarea.value;
-						if (value === this.content) {
-							notify(null, E('p', _('No modifications detected. The content remains unchanged.')), 3000);
-						} else {
-							this.handleFileSave(path, value.trim().replace(/\r\n/g, '\n') + '\n');
-						};
-					})
-				}, _('Save')),
-				E('div', {
-					class: 'btn cbi-button-reset',
-					click: ui.createHandlerFn(this, () => dom.textarea.value = this.content)
-				}, _('Reset'))
-			])
 		]);
 		setTimeout(() => {
-			dom.select = document.querySelector('select');
-			dom.textarea = document.querySelector('textarea');
+			d.textarea = document.querySelector('textarea');
+			d.actions = document.querySelector('.cbi-page-actions');
+			if (d.actions) d.actions.style.display = 'none';
 		}, 0);
 		return view;
 	},
 
-	handleFileSave: (path, value) => {
-		fs.write(path, value).then(() => {
-			notify(null, E('p', '%s %s'.format(path, _('Contents have been saved.'))), 5000, 'info');
-			const serviceMap = {
-				wireless: { cmd: '/sbin/wifi', args: ['reload'] },
-				dhcp: { cmd: '/etc/init.d/dnsmasq', args: ['reload'] },
-				crontabs: { cmd: '/etc/init.d/cron', args: ['reload'] },
-				hosts: { cmd: '/etc/init.d/dnsmasq', args: ['reload'] },
-				uhttpd: { cmd: '/etc/init.d/uhttpd', args: ['reload'] },
-				network: { cmd: '/etc/init.d/network', args: ['reload'] },
-				dnsmasq: { cmd: '/etc/init.d/dnsmasq', args: ['reload'] },
-				firewall: { cmd: '/etc/init.d/firewall', args: ['reload'] }
-			};
+	handleSaveApply: function () {
+		if (d.oldcontent === d.textarea.value)
+			return notify(null, E('p', _('No modifications detected. The content remains unchanged.')), 3000);
+		d.path && fs.write(d.path, d.textarea.value).then(() => {
+			notify(null, E('p', '%s %s'.format(d.path, _('Contents have been saved.'))), 5000, 'info');
+			const serviceMap = [
+				{ key: 'wireless', cmd: '/sbin/wifi' },
+				{ key: 'crontabs', cmd: '/etc/init.d/cron' },
+				{ key: 'uhttpd', cmd: '/etc/init.d/uhttpd' },
+				{ key: 'dhcp', cmd: '/etc/init.d/dnsmasq' },
+				{ key: 'hosts', cmd: '/etc/init.d/dnsmasq' },
+				{ key: 'network', cmd: '/etc/init.d/network' },
+				{ key: 'dnsmasq', cmd: '/etc/init.d/dnsmasq' },
+				{ key: 'firewall', cmd: '/etc/init.d/firewall' }
+			];
 
-			const service = Object.entries(serviceMap).find(([key]) => path.includes(key));
-			if (service) fs.exec(service[1].cmd, service[1].args).then(res =>
-				res.code === 0
-					? notify(null, E('p', _('Service %s reloaded successfully.').format(service[1].cmd)), 5000, 'info')
-					: notify(null, E('p', _('Service reload failed: %s').format(e.message)), 5000, 'warning'));
-		}).catch(e =>
-			notify(null, E('p', _('Unable to save contents: %s').format(e.message)), 5000, 'error'));
+			const service = serviceMap.find(({ key }) => d.path.includes(key));
+			if (service) {
+				fs.exec(service.cmd, ['reload']).then((res) =>
+					res.code === 0
+						? notify(null, E('p', _('Service %s reloaded successfully.').format(service.cmd)), 5000, 'info')
+						: notify(null, E('p', _('Service reload failed: %s').format(e.message)), 5000, 'warning')
+				);
+			};
+		}).catch((e) =>
+			notify(null, E('p', _('Unable to save contents: %s').format(e.message)), 5000, 'error')
+		);
 	},
 
-	handleSave: null,
-	handleReset: null,
-	handleSaveApply: null
+	handleSave: function () {
+		if (d.oldcontent === d.textarea.value)
+			return notify(null, E('p', _('No modifications detected. The content remains unchanged.')), 3000);
+		d.path && fs.write(d.path, d.textarea.value)
+			.then(() => notify(null, E('p', '%s %s'.format(d.path, _('Contents have been saved.'))), 5000, 'info'))
+			.catch(e => notify(null, E('p', _('Unable to save contents: %s').format(e.message)), 5000, 'error'));
+	},
+
+	handleReset: function () {
+		d.path && (document.querySelector('textarea').value = d.oldcontent);
+	},
 });
